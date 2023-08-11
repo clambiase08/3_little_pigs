62fc1598c53d6d8c5dd9874e8f5654567365ff7d
Christina Lambiase <christina.lambiase@gmail.com>
2023-08-10 17:14:32 -0700

    bug squashed

--- lib/cli.py
+++ lib/cli.py
@@ -7,21 +7,29 @@ from helpers import *
 import time
 import os
 
-engine = create_engine('sqlite:///db/3_little_pigs.db')
+engine = create_engine("sqlite:///lib/db/3_little_pigs.db")
 session = sessionmaker(bind=engine)()
 
 CURRENT_STORY = None
 CURRENT_SCENE = None
 
+
 def display_welcome():
-     print_slowly("3 Little Pigs - choose your own adventure")
+    print_slowly("3 Little Pigs - choose your own adventure")
+
 
 def display_main_menu():
-
-        print_slowly("Way back in Once upon a time time, there were three little pigs, all siblings. The first little big, Albert, was VERY lazy. He didn't want to work very hard so he wants to build his house out of straw.")
-        print_slowly("The second little pig, Brenda, worked a little bit harder but was still a little lazy so she wants to build her house out of twigs.")
-        print_slowly("The third little pig, Charlie, worked very hard and so he wants to build his house out of bricks.")
-        print_kinda_slow("""
+    print_slowly(
+        "Way back in Once upon a time time, there were three little pigs, all siblings. The first little big, Albert, was VERY lazy. He didn't want to work very hard so he wants to build his house out of straw."
+    )
+    print_slowly(
+        "The second little pig, Brenda, worked a little bit harder but was still a little lazy so she wants to build her house out of twigs."
+    )
+    print_slowly(
+        "The third little pig, Charlie, worked very hard and so he wants to build his house out of bricks."
+    )
+    print_kinda_slow(
+        """
                                                           (
                                                            (  )
                                   .____.             ,-.  __)
@@ -33,9 +41,11 @@ def display_main_menu():
             ((|||(oo)|||)     |||||(oo)|||||    |   |'  |   |  
                 Albert           Brenda          Charlie                                            
                                                                                              
-                """)
-        print("First you get to pick which pig you'd like to be:")
-        print_kinda_slow("""
+                """
+    )
+    print("First you get to pick which pig you'd like to be:")
+    print_kinda_slow(
+        """
                _____
             ^..^     \9
             (oo)_____/
@@ -45,26 +55,29 @@ def display_main_menu():
             A = Albert, a straw house
             B = Brenda, a twig house
             C = Charlie, a brick house
-            """)
-        pigchoice = input('>>> ')
-        handle_pig_choice(pigchoice)
+            """
+    )
+    pigchoice = input()
+    handle_pig_choice(pigchoice)
+
 
 def handle_pig_choice(pigchoice):
-        pigname=""
-        if pigchoice == "A":
-            pigname = "Albert"
-        elif pigchoice == "B":
-            pigname = "Brenda"
-        else:
-            pigname = "Charlie"
-        pig = get_pig(pigname)
-        save_pig(pig)
-        choose_your_neighbor(pig)
-        return pig
+    pigname = ""
+    if pigchoice.lower() == "a":
+        pigname = "Albert"
+    elif pigchoice.lower() == "b":
+        pigname = "Brenda"
+    else:
+        pigname = "Charlie"
+    pig = get_pig(pigname)
+    save_pig(pig)
+    print(pig.name)
+    choose_your_neighbor(pig)
+
 
 def choose_your_neighbor(pig):
-
-        print_kinda_slow("""
+    print_kinda_slow(
+        """
                                .
                               / V\
                             / `  /
@@ -81,42 +94,47 @@ def choose_your_neighbor(pig):
               Wolf:
               A = THE Wolf
               B = BB Wolf
-              """)
-        wolfchoice = input('>>> ')
-        return pig, wolfchoice
+              """
+    )
+    wolfchoice = input()
+    handle_neighbor_choice(pig, wolfchoice)
+    # return pig, wolfchoice
+
 
 def handle_neighbor_choice(pig, wolfchoice):
-        wolfname=""
-        if wolfchoice == "A":
-            wolfname = "THE Wolf"
-        else:
-            wolfname = "BB Wolf"
-        wolf = get_wolf(wolfname)
-        save_wolf(wolf)
-        # set_up_story(pig, wolf)
-        return pig, wolf
+    wolfname = ""
+    if wolfchoice.lower() == "a":
+        wolfname = "THE Wolf"
+    else:
+        wolfname = "BB Wolf"
+    wolf = get_wolf(wolfname)
+    save_wolf(wolf)
+    set_up_story(pig, wolf)
+    # return pig, wolf
+
 
 def set_up_story(pig, wolf):
-        start_scene = 4
-        if (pig.name == "Albert" or pig.name == "Brenda") and wolf.name == "THE Wolf":
-             start_scene = 5
-        elif (pig.name == "Albert" or pig.name == "Brenda") and wolf.name == "BB Wolf":
-             start_scene = 6
-        elif pig.name == "Charlie" and wolf.name == "THE Wolf":
-             start_scene = 15
-        elif pig.name == "Charlie" and wolf.name == "BB Wolf":
-             start_scene = 16
-        scene_1 = session.query(Scene).filter(Scene.scene_num == start_scene).first()
-        story = Story(hero_id = pig.id, antagonist_id = wolf.id)
-        #also want the option to go back, deleting the last decision made from the storyline
-        session.add(story)
-        session.commit()
-        storyline = Storyline(story_id = story.id, scene_id = scene_1.id)
-        global CURRENT_STORY
-        global CURRENT_SCENE
-        CURRENT_STORY = story
-        CURRENT_SCENE = scene_1
-        display_current_scene()
+    start_scene = 4
+    if (pig.name == "Albert" or pig.name == "Brenda") and wolf.name == "THE Wolf":
+        start_scene = 5
+    elif (pig.name == "Albert" or pig.name == "Brenda") and wolf.name == "BB Wolf":
+        start_scene = 6
+    elif pig.name == "Charlie" and wolf.name == "THE Wolf":
+        start_scene = 15
+    elif pig.name == "Charlie" and wolf.name == "BB Wolf":
+        start_scene = 16
+    scene_1 = session.query(Scene).filter(Scene.scene_num == start_scene).first()
+    story = Story(hero_id=pig.id, antagonist_id=wolf.id)
+    # also want the option to go back, deleting the last decision made from the storyline
+    session.add(story)
+    session.commit()
+    storyline = Storyline(story_id=story.id, scene_id=scene_1.id)
+    global CURRENT_STORY
+    global CURRENT_SCENE
+    CURRENT_STORY = story
+    CURRENT_SCENE = scene_1
+    display_current_scene()
+
 
 def display_current_scene():
     os.system("clear")
@@ -126,28 +144,38 @@ def display_current_scene():
     print_slowly(f"A - {CURRENT_SCENE.choice_A}")
     print_slowly(f"B - {CURRENT_SCENE.choice_B}")
     print_slowly("C - go back")
-    choice = input('>>> ')
+    choice = input(">>> ")
     if choice == "C":
-         os.system("clear")
-         print("Going back")
+        os.system("clear")
+        print("Going back")
     elif choice == "A":
         os.system("clear")
-        CURRENT_SCENE = session.query(Scene).filter(Scene.scene_num == CURRENT_SCENE.choice_A_next_scene).first()
-        Storyline(story_id = CURRENT_STORY.id, scene_id = CURRENT_SCENE.id)
+        CURRENT_SCENE = (
+            session.query(Scene)
+            .filter(Scene.scene_num == CURRENT_SCENE.choice_A_next_scene)
+            .first()
+        )
+        Storyline(story_id=CURRENT_STORY.id, scene_id=CURRENT_SCENE.id)
         display_current_scene()
-        
+
     else:
         os.system("clear")
-        CURRENT_SCENE = session.query(Scene).filter(Scene.scene_num == CURRENT_SCENE.choice_B_next_scene).first()
-        Storyline(story_id = CURRENT_STORY.id, scene_id = CURRENT_SCENE.id)
+        CURRENT_SCENE = (
+            session.query(Scene)
+            .filter(Scene.scene_num == CURRENT_SCENE.choice_B_next_scene)
+            .first()
+        )
+        Storyline(story_id=CURRENT_STORY.id, scene_id=CURRENT_SCENE.id)
         display_current_scene()
 
-def the_end():
-     pass
 
-#scene 7
-#print("""
-    
+def the_end():
+    pass
+
+
+# scene 7
+# print("""
+
 #                         (
 #                           )     (
 #                    ___...(-------)-....___
@@ -170,29 +198,29 @@ def the_end():
 #        '._     """----.....______.....----"""     _.'
 #           `""--..,,_____            _____,,..--""`
 #                         `"""----"""`
-#  or 
+#  or
 #     ( (
 #      ) )
 #   ........
 #  |        |]
-#   \      /    
+#   \      /
 #    `----'
 
 # """)
 # scene 19
-    #           (
-    #            )  )
-    #        ______(____
-    #       (___________)
-    #       /           \
-    #      /             \
-    #     |               |
-    #  ____\             /____
-    # ()____'.__     __.'____()
-    #      .'` .'```'. `-.
-    #     ().'`       `'.()
+#           (
+#            )  )
+#        ______(____
+#       (___________)
+#       /           \
+#      /             \
+#     |               |
+#  ____\             /____
+# ()____'.__     __.'____()
+#      .'` .'```'. `-.
+#     ().'`       `'.()
 
 if __name__ == "__main__":
     display_welcome()
     while True:
-        display_main_menu() 
+        display_main_menu()
--- lib/helpers.py
+++ lib/helpers.py
@@ -4,42 +4,49 @@ from sqlalchemy.orm import sessionmaker
 from db.models import Character, Scene, Story
 import time
 
-engine = create_engine('sqlite:///db/3_little_pigs.db')
+engine = create_engine("sqlite:///lib/db/3_little_pigs.db")
 session = sessionmaker(bind=engine)()
+
 
 def print_slowly(output):
     for char in output:
-        print(char, end='', flush=True)
+        print(char, end="", flush=True)
         time.sleep(0.02)
         # time.sleep(0)
     print()
 
+
 def print_kinda_slow(output):
     for char in output:
-        print(char, end='', flush=True)
+        print(char, end="", flush=True)
         time.sleep(0.008)
         # time.sleep(0)
     print()
 
+
 def print_rapidly(output):
     for char in output:
-        print(char, end='', flush=True)
+        print(char, end="", flush=True)
         time.sleep(0.004)
         # time.sleep(0)
     print()
+
 
 def save_pig(character):
     new_story = Story(title="", hero_id=character.id, antagonist_id=None)
     session.add(new_story)
     session.commit()
 
+
 def save_wolf(character):
     character.antagonist_id = character.id
     session.commit()
 
+
 def get_pig(pigname):
     pig = session.query(Character).filter(Character.name == pigname).first()
     return pig
+
 
 def get_wolf(wolfname):
     wolf = session.query(Character).filter(Character.name == wolfname).first()
